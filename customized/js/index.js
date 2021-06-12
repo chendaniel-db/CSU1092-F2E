75c49fba18844f0f2b8d38ddec41776db2b8d0ee
Daniel Chen <chen.0910705@gmail.com>
2021-06-12 20:03:48 +0800

    20210612200344

--- customized/js/index.js
+++ customized/js/index.js
@@ -35,4 +35,4 @@ function ShowTime()
             var dayNames = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
             document.getElementById('showbox').innerHTML = '系統目前時間：' + NowDate.toLocaleString() + '（' + dayNames[d] + '）';
             setTimeout('ShowTime()', 1);
-        }
+}
