bf8f6ff83d88b07eeaa09f0d6c829e359278ae1c
Daniel Chen <chen.0910705@gmail.com>
2021-06-12 15:44:06 +0800

    20211612154402

--- customized/js/Booking.js
+++ customized/js/Booking.js
@@ -67,3 +67,12 @@ $.getJSON("https://ipinfo.io", function (response) {
 
     $("#myip").html(response.ip);
 })
+
+function ShowTime()
+        {
+            var NowDate = new Date();
+            var d = NowDate.getDay();
+            var dayNames = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+            document.getElementById('showbox').innerHTML = '系統目前時間：' + NowDate.toLocaleString() + '（' + dayNames[d] + '）';
+            setTimeout('ShowTime()', 1000);
+        }
