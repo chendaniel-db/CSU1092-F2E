1a3c2c4f34250afde726d7c0aeb116a2fbc61ae1
Daniel Chen <chen.0910705@gmail.com>
2021-06-12 15:30:50 +0800

    20210612153000

--- customized/js/About.js
+++ customized/js/About.js
@@ -3,3 +3,20 @@ $.getJSON("https://ipinfo.io", function (response) {
 
     $("#myip").html(response.ip);
 })
+
+function LoveMessenger() {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: '感謝您的支持',
+        text: '我已經用心接受到您傳達的心意囉！',
+        showConfirmButton: false,
+        timer: 2000,
+        showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+            popup: 'animate__animated animate__fadeOutUp'
+        },
+    })
+}
